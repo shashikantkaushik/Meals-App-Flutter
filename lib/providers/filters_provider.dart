c9aee2a49774d8fc8f8ccacfd1ce72ca0736cd63
Shashi Kant <shashikantkaushik4@gmail.com>
2023-06-27 01:29:44 +0530

    favourites, steps, ui, logic

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -4,4 +4,4 @@ class FilterNotifier extends StateNotifier<Map<Filter,bool>>{
 FiltersNotifier():super();
 }
 
-final filtersProvider=StateNotifierProvider((ref) => );
+final filtersProvider=StateNotifierProvider((ref) => ); m
