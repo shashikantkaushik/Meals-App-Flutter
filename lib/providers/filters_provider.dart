2a051bd9ddfaca03c311c0933326b0321b495034
Shashi Kant <shashikantkaushik4@gmail.com>
2023-06-27 11:25:56 +0530

    animation add

--- lib/providers/filters_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:riverpod/riverpod.dart';
-
-class FilterNotifier extends StateNotifier<Map<Filter,bool>>{
-FiltersNotifier():super();
-}
-
-final filtersProvider=StateNotifierProvider((ref) => ); m
--- lib/providers/meals_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:meals/data/dummy_data.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-final mealsProvider=Provider((ref){
-  return dummyMeals;
-});
